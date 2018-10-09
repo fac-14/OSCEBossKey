a3f8bc0a1405e2e67c84e3059ded76ecac0ca413
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-09 11:49:22 +0100

    removed console.log Relates #58

--- src/utils/HistoryCaseRevision.functions.js
+++ src/utils/HistoryCaseRevision.functions.js
@@ -1,30 +1,17 @@
 const functions = {
   markComplete: (id, prevState) => {
-    console.log("this is id", id);
-    console.log(
-      "this is completed before click",
-      prevState.markSchemeElements[id].completed
-    );
     if (!prevState.markSchemeElements[id]) {
       throw new Error("element out of range");
     }
     const markSchemeElements = [...prevState.markSchemeElements];
     if (!markSchemeElements[id].completed) {
       markSchemeElements[id].completed = true;
-      console.log(
-        "this is completed after click",
-        markSchemeElements[id].completed
-      );
       return {
         markSchemeElements,
         markSchemeCompleted: prevState.markSchemeCompleted + 1
       };
     }
     markSchemeElements[id].completed = false;
-    console.log(
-      "this is completed after click",
-      markSchemeElements[id].completed
-    );
     return {
       markSchemeElements,
       markSchemeCompleted: prevState.markSchemeCompleted - 1
