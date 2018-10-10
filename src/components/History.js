38763d5a89d00a3871521e6345fb34cd94d46019
BryMon <monika.flisowska@gmail.com>
2018-10-10 11:16:17 +0100

    build history page, render history stations, Relates #88

--- src/components/History.js
+++ src/components/History.js
@@ -1,14 +1,37 @@
 /* eslint-disable class-methods-use-this */
 import React from "react";
+import { withRouter } from "react-router";
+import PropTypes from "prop-types";
+
+import dummyData from "../dummy-data.json";
+
+import StationName from "./StationName";
+import AddNewCase from "./AddNewCase";
 import Navbar from "./Navbar";
 
-export default class History extends React.Component {
+class History extends React.Component {
+  state = {
+    stations: dummyData.history
+  };
+
   render() {
+    const stationElements = this.state.stations.map((element, index) => (
+      <div key={index}>{element.stationName}</div>
+    ));
     return (
-      <div>
-        <h1>History Stations</h1>
+      <React.Fragment>
+        <h1>History</h1>
+        <p>Add new</p>
+        {stationElements}
         <Navbar />
-      </div>
+      </React.Fragment>
     );
   }
 }
+
+History.propTypes = {
+  cases: PropTypes.arrayOf(PropTypes.object),
+  match: PropTypes.object
+};
+
+export default withRouter(History);
