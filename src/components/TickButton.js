31d6095d0c147349262ae4a3a20f169698d75daf
BryMon <monika.flisowska@gmail.com>
2018-10-10 11:53:59 +0100

    add link to tick button on reviosion page to render stats, Relates #88

--- src/components/TickButton.js
+++ src/components/TickButton.js
@@ -4,19 +4,22 @@ import "../assets/tick-button.scss";
 
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default class TickButton extends React.Component {
   render() {
     return (
-      <button
-        id="button--tick"
-        onClick={() => this.props.submitCase()}
-        className={
-          this.props.tickDisplayed ? `tick--displayed` : `tick--not-displayed`
-        }
-      >
-        Ticked
-      </button>
+      <Link to="/stats">
+        <button
+          id="button--tick"
+          onClick={() => this.props.submitCase()}
+          className={
+            this.props.tickDisplayed ? `tick--displayed` : `tick--not-displayed`
+          }
+        >
+          Ticked
+        </button>
+      </Link>
     );
   }
 }
