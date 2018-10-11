7c3137a600d38210cef641bd0e424ccb731bb3bb
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-11 15:18:09 +0100

    add ExamName component

---
+++ src/components/ExamName.js
@@ -0,0 +1,15 @@
+/* eslint-disable class-methods-use-this */
+import "../assets/exam-name.scss";
+
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class ExamName extends React.Component {
+  render() {
+    return <h1 id="header--exam-name">{this.props.exam}</h1>;
+  }
+}
+
+ExamName.propTypes = {
+  exam: PropTypes.string
+};
