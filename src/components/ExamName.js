722aab87a7102a9930bab43c4eb9bd8bcf7228fb
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-11 16:57:48 +0100

    add scss import in ExamName, but comment out contents of scss file

--- src/components/ExamName.js
+++ src/components/ExamName.js
@@ -1,5 +1,5 @@
 /* eslint-disable class-methods-use-this */
-// import "../assets/exam-name.scss";
+import "../assets/exam-name.scss";
 
 import React from "react";
 import PropTypes from "prop-types";
