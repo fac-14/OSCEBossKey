ccecc03175cb973370248ef6b632096bdf400c40
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-11 15:14:13 +0100

    add AddNewStation component

---
+++ src/components/AddNewStation.js
@@ -0,0 +1,14 @@
+/* eslint-disable class-methods-use-this */
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class AddNewStation extends React.Component {
+  render() {
+    return (
+      <Link to="/history/new-station" id="add-station">
+        <div className="stations">Add Station</div>
+      </Link>
+    );
+  }
+}
