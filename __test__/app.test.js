fa6666f99476f1cf18043ea8d6210a922e225639
martingaston <squidman@gmail.com>
2018-10-12 12:46:30 +0100

    API TDD: add test for /api/history/ endpoint

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -4,8 +4,24 @@ const app = require("../src/app");
 test("GET /database-test returns list of station names", done => {
   request(app)
     .get("/database-test/")
+    .expect(200)
     .then(res => {
       expect(res.statusCode).toBe(200);
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+      done(err);
+    });
+});
+
+test("GET /api/history returns list of station names as a JSON object", done => {
+  request(app)
+    .get("/api/history/")
+    .expect(200)
+    .expect("Content-Type", /json/)
+    .then(res => {
+      expect(typeof res).toBe("object");
       done();
     })
     .catch(err => {
