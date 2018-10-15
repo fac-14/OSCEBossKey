75050951a690296efcb81843db2f46951afdbf9f
squidmania <squidman@gmail.com>
2018-10-15 21:19:28 +0100

    Add API mocks to React component tests Relates #112

--- __test__/HistoryCaseRevision.test.js
+++ __test__/HistoryCaseRevision.test.js
@@ -3,9 +3,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Revision from "../src/components/Pages/Revision/RevisionPage";
 import { render, fireEvent } from "react-testing-library";
 
+// setup a mock API call with fetch-mock to provide <Revision> component with correct information
+const fetchMock = require("fetch-mock");
+fetchMock.mock("end:/api/history/chest-pain/case/1", {
+  payload: {
+    title: "32-year-old man has panic attack after mocking fetch() in Node",
+    details:
+      "poor 32-year-old man is very thankful for fetch-mock and node-fetch packages",
+    mark_scheme: ["one", "two", "three"]
+  }
+});
+
 const { getByTestId } = render(
   <Router>
-    <Revision match={{ params: { station: "chest-pain", caseid: "0" } }} />
+    <Revision match={{ params: { station: "chest-pain", caseid: "1" } }} />
   </Router>
 );
 
