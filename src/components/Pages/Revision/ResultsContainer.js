24f8a3940fe20182266642d9d385478e70e74c8f
martingaston <squidman@gmail.com>
2018-10-17 08:14:46 +0100

    Rename class to className in ResultsContainer Relates #12

--- src/components/Pages/Revision/ResultsContainer.js
+++ src/components/Pages/Revision/ResultsContainer.js
@@ -5,21 +5,20 @@ import PropTypes from "prop-types";
 import RevisionTitle from "./RevisionTitle";
 
 export default class ResultsContainer extends React.Component {
-  state = {};
   render() {
     return (
       <div id="revision">
         <RevisionTitle caseTitle="Results" />
         <div id="revision-container">
           <ul id="revision-list">
-            <li class="result-list-item" id="score-percent">
+            <li className="result-list-item" id="score-percent">
               {Math.floor(
                 (this.props.markSchemeCompleted / this.props.markSchemeTotal) *
                   100
               )}
               %
             </li>
-            <li class="result-list-item">
+            <li className="result-list-item">
               You performed {this.props.markSchemeCompleted} of{" "}
               {this.props.markSchemeTotal} tasks.
             </li>
