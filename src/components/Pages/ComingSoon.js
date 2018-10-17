9046921923004b53f6646f56102696e85d13d1d4
martingaston <squidman@gmail.com>
2018-10-17 17:18:34 +0100

    Refactor ComingSoon Relates #125

--- src/components/Pages/ComingSoon.js
+++ src/components/Pages/ComingSoon.js
@@ -6,12 +6,10 @@ import BackButton from "../TopBar/BackButton";
 export default class ComingSoon extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <div id="topbar-container">
-          <BackButton link="history" />
-          <h3 id="topbar-title">Coming soon...</h3>;
-        </div>
-      </React.Fragment>
+      <div id="topbar-container">
+        <BackButton link="history" />
+        <h3 id="topbar-title">Coming soon...</h3>;
+      </div>
     );
   }
 }
