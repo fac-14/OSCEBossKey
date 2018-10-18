d1ee10e2eb35b92c3a3dec82ca3aa353d737c5d2
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-18 13:15:32 +0100

    add style to instructionH2 in AddNewFlow

--- src/components/Pages/AddNew/NewTileInput.js
+++ src/components/Pages/AddNew/NewTileInput.js
@@ -1,15 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import InstructionText from "./InstructionText";
 import NewTileTextInput from "./NewTileTextInput";
+
+const StyledNewTileInputContainer = styled.div`
+  background: blue;
+  margin-bottom: 16px;
+`;
 
 export default class NewTileInput extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <InstructionText text={this.props.instructionText} />
-        <NewTileTextInput userTypes={this.props.userTypes} />
+        <StyledNewTileInputContainer>
+          <InstructionText text={this.props.instructionText} />
+          <NewTileTextInput userTypes={this.props.userTypes} />
+        </StyledNewTileInputContainer>
       </React.Fragment>
     );
   }
--- src/components/Pages/AddNew/NewTileTextInput.js
+++ src/components/Pages/AddNew/NewTileTextInput.js
@@ -1,5 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const NewTileTextInputContainer = styled.div`
+  background-color: red;
+`;
 
 export default class NewTileTextInput extends React.Component {
   state = {
