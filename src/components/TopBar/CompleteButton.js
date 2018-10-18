744d25702adce90c95e0bec57f00edaec815afdd
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-10-18 18:52:35 +0100

    convert TopBar to styled component, relates #148

--- src/components/TopBar/CompleteButton.js
+++ src/components/TopBar/CompleteButton.js
@@ -3,7 +3,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import tickIcon from "../../assets/icons/tick_white.svg";
-import { Link } from "react-router-dom"; /* eslint-disable-line */
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 0 0 0 16px;
+  border: none;
+  background-color: transparent;
+`;
+
+const StyledImg = styled.img`
+  height: 20px;
+`;
 
 export default class CompleteButton extends React.Component {
   render() {
@@ -12,20 +23,16 @@ export default class CompleteButton extends React.Component {
     if (this.props.addNewFlow) {
       component = (
         <Link to={`/${exam}/${station}`}>
-          <button
-            id="complete"
-            data-testid="complete"
-            onClick={() => onClick()}
-          >
-            <img src={tickIcon} />
-          </button>
+          <StyledButton data-testid="complete" onClick={() => onClick()}>
+            <StyledImg src={tickIcon} />
+          </StyledButton>
         </Link>
       );
     } else {
       component = (
-        <button id="complete" data-testid="complete" onClick={() => onClick()}>
-          <img src={tickIcon} />
-        </button>
+        <StyledButton data-testid="complete" onClick={() => onClick()}>
+          <StyledImg src={tickIcon} />
+        </StyledButton>
       );
     }
 
