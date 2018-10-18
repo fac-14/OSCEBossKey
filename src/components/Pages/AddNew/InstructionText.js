99181837921da310d4126f2d7cccf22391d30be1
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-18 11:39:10 +0100

    add styles for instruction text

--- src/components/Pages/AddNew/InstructionText.js
+++ src/components/Pages/AddNew/InstructionText.js
@@ -1,9 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledInstructionH2 = styled.h2`
+  font-family: "Roboto", Helvetica, sans-serif;
+  font-weight: 700;
+`;
 
 export default class InstructionText extends React.Component {
   render() {
-    return <h2 id="revision-title">{this.props.text}</h2>;
+    return (
+      <StyledInstructionH2 id="revision-title">
+        {this.props.text}
+      </StyledInstructionH2>
+    );
   }
 }
 
