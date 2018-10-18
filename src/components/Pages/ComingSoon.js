6ee17dcc000d7e2556e851b5806e6632eebc3350
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-10-18 18:33:20 +0100

    convert ComingSoon to styled component, relates #148

--- src/components/Pages/ComingSoon.js
+++ src/components/Pages/ComingSoon.js
@@ -3,14 +3,29 @@
 import React from "react";
 import NavBar from "../Navbar/Navbar";
 import comingSoonIcon from "../../assets/icons/coming-soon-icon.svg";
+import styled from "styled-components";
+
+const ComingSoonDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  width: 100vw;
+  margin: 64px 0;
+`;
+
+const ComingSoonImg = styled.img`
+  width: 80%;
+  max-height: 100%;
+`;
 
 export default class ComingSoon extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div id="icon-wrapper">
-          <img id="coming-soon-icon" src={comingSoonIcon} />
-        </div>
+        <ComingSoonDiv>
+          <ComingSoonImg id="coming-soon-icon" src={comingSoonIcon} />
+        </ComingSoonDiv>
         <NavBar />
       </React.Fragment>
     );
