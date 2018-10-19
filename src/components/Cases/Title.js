bc357716d111d704d482cd9d941c7e4684d3e27e
BryMon <monika.flisowska@gmail.com>
2018-10-19 11:15:56 +0100

    make addnewbanner a functional component, relates #154

--- src/components/Cases/Title.js
+++ src/components/Cases/Title.js
@@ -5,8 +5,8 @@ import PropTypes from "prop-types";
 
 const CasesPageTitle = props => <h1 id="title">{props.stationName}</h1>;
 
-export default CasesPageTitle;
-
 CasesPageTitle.propTypes = {
   stationName: PropTypes.string
 };
+
+export default CasesPageTitle;
