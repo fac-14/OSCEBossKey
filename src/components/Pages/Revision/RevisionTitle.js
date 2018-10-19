efd5e0a0e57fc9491e7b2b3f8c41806f09e1b0fb
BryMon <monika.flisowska@gmail.com>
2018-10-19 11:33:58 +0100

    fix props for revisionTitle

--- src/components/Pages/Revision/RevisionTitle.js
+++ src/components/Pages/Revision/RevisionTitle.js
@@ -3,9 +3,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const RevisionTitle = props => (
-  <h2 id="revision-title">{this.props.caseTitle}</h2>
-);
+const RevisionTitle = props => <h2 id="revision-title">{props.caseTitle}</h2>;
 
 RevisionTitle.propTypes = {
   caseTitle: PropTypes.string
