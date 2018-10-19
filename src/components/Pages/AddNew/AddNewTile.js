c0b425121b839f4e3311cd9af20cd39516ae76f0
BryMon <monika.flisowska@gmail.com>
2018-10-19 11:17:51 +0100

    make AddNewTile a functional component, relates #154

--- src/components/Pages/AddNew/AddNewTile.js
+++ src/components/Pages/AddNew/AddNewTile.js
@@ -3,12 +3,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default class AddNewTile extends React.Component {
-  render() {
-    return (
-      <Link to="/history/add/station" id="add-station">
-        <div className="tile">Add New</div>
-      </Link>
-    );
-  }
-}
+const AddNewTile = () => (
+  <Link to="/history/add/station" id="add-station">
+    <div className="tile">Add New</div>
+  </Link>
+);
+
+export default AddNewTile;
