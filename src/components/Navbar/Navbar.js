dda82252c6a39c9aa365bb87ed2ee4fef5596cce
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-10-19 11:35:14 +0100

    changed the navbar into styled components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,35 +4,73 @@ import historyIcon from "../../assets/icons/history-black.svg";
 import examinationIcon from "../../assets/icons/examinations-black.svg";
 import extrasIcon from "../../assets/icons/extras-black.svg";
 import statisticsIcon from "../../assets/icons/statistics-black.svg";
+import styled from "styled-components";
+
+const StyledUL = styled.ul`
+  background: #c7c7b2;
+  display: flex;
+  width: 100vw;
+  position: fixed;
+  bottom: 0;
+`;
+
+const StyledLI = styled.li`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const StyledLink = styled(Link)`
+  padding: 4px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledImg = styled.img`
+  width: 24px;
+  height: 24px;
+  margin: 4px;
+`;
+
+const StyledText = styled.h3`
+  font-family: "Roboto", Helvetica, sans-serif;
+  font-size: 8px;
+  color: black;
+  text-transform: uppercase;
+  font-weight: bold;
+`;
 
 const Navbar = () => (
   <div>
-    <ul id="navbar">
-      <li className="history-li">
-        <Link to="/history">
-          <img id="history-icon" src={historyIcon} />
-          <h3 className="text">History</h3>
-        </Link>
-      </li>
-      <li className="examinations-li">
-        <Link to="/examinations">
-          <img id="examinations-icon" src={examinationIcon} />
-          <h3 className="text">Examinations</h3>
-        </Link>
-      </li>
-      <li className="extras-li">
-        <Link to="/extras">
-          <img id="extras-icon" src={extrasIcon} />
-          <h3 className="text">Extras</h3>
-        </Link>
-      </li>
-      <li className="stats-li">
-        <Link to="/stats">
-          <img id="statistics-icon" src={statisticsIcon} />
-          <h3 className="text">Statistics</h3>
-        </Link>
-      </li>
-    </ul>
+    <StyledUL>
+      <StyledLI>
+        <StyledLink to="/history">
+          <StyledImg src={historyIcon} />
+          <StyledText>History</StyledText>
+        </StyledLink>
+      </StyledLI>
+      <StyledLI>
+        <StyledLink to="/examinations">
+          <StyledImg src={examinationIcon} />
+          <StyledText>Examinations</StyledText>
+        </StyledLink>
+      </StyledLI>
+      <StyledLI>
+        <StyledLink to="/extras">
+          <StyledImg src={extrasIcon} />
+          <StyledText>Extras</StyledText>
+        </StyledLink>
+      </StyledLI>
+      <StyledLI>
+        <StyledLink to="/stats">
+          <StyledImg src={statisticsIcon} />
+          <StyledText>Statistics</StyledText>
+        </StyledLink>
+      </StyledLI>
+    </StyledUL>
   </div>
 );
 
